d3cb44667e28391dceb734b896666d50e1785d76
Daniel <daniel.ramirezv.col@upb.edu.co>
2025-02-18 09:53:51 -0500

    Solucion de errores con la API. Se usa API 35 para correr el codigo

--- app/src/main/java/com/upb/mirestaurante1/MenuActivity.java
+++ app/src/main/java/com/upb/mirestaurante1/MenuActivity.java
@@ -1,6 +1,9 @@
 package com.upb.mirestaurante1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -11,7 +14,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     String [] Categorias = new String [] {"Entradas","Platos Fuertes", "Bebidas", "Postres"};
 
@@ -25,5 +28,24 @@ public class MenuActivity extends AppCompatActivity {
         ListAdapter la = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,Categorias);
         //Asigna contenido a la vista
         lv.setAdapter(la);
+        lv.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+        Intent intencion=null;
+        switch (i){
+
+            case 0:  intencion = new Intent(this, EntradasActivity.class);
+                    startActivity(intencion);
+                    break;
+
+            case 1:  intencion = new Intent(this, PlatosActivity.class);
+                    startActivity(intencion);
+                    break;
+
+            // Insertar otros productos
+        }
     }
 }
